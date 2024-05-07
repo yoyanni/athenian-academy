e4a1ff1e47c0ed83a46625d5f87eda78e25a97bb
yoyanni <yanni.alevras@gmail.com>
2024-05-08 00:05:29 +0300

    Implement a rough responsive design for Navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { mainMenuResponse } from "../types";
 
 import greekFlag from "../../../public/Layout/Navbar/greek-flag.png";
+import burger from "../../../public/Layout/Navbar/burger.svg";
 import downArrow from "../../../public/Layout/Navbar/down-arrow.svg";
 import search from "../../../public/Layout/Navbar/search-icon.svg";
 import logo from "../../../public/Layout/logo.png";
@@ -31,22 +32,27 @@ const Navbar = () => {
   return (
     <header>
       <div className="h-[100px] flex justify-center items-center bg-primaryDark">
-        <Image src={logo} alt="search" width={264} />
+        <Image className="ms-auto translate-x-[20px]" src={logo} alt="search" width={264} />
+        <Image className="ms-auto me-4" src={burger} alt="search" width={40} />
       </div>
-      <nav className="h-[80px] flex justify-center items-center gap-4 bg-primary text-navTextColor">
+      <nav className="flex flex-col justify-center items-center sm:gap-4 bg-primary text-navTextColor sm:h-[80px] sm:flex-row">
         {data && (
           <>
-            <ul className="flex justify-center gap-8">
+            <ul className="w-full flex flex-col justify-center sm:flex-row sm:gap-8">
               {data.items.map((i) => {
                 const linkClasses =
                   activeItem === i.id
                     ? "bg-white text-primary"
                     : "hover:bg-footerIconBackground";
                 return (
-                  <li key={i.id} onClick={() => setActiveItem(i.id)}>
+                  <li
+                    key={i.id}
+                    className="text-center"
+                    onClick={() => setActiveItem(i.id)}
+                  >
                     <Link
                       href={i.url}
-                      className={`p-2 rounded-full ${linkClasses}`}
+                      className={`block py-4 border-b sm:border-0 sm:p-2 sm:rounded-full  ${linkClasses}`}
                     >
                       {i.name}
                     </Link>
@@ -54,17 +60,19 @@ const Navbar = () => {
                 );
               })}
             </ul>
-            <button>
-              <Image src={search} alt="search-bar icon" />
-            </button>
-            <div className="flex gap-1">
-              <Image
-                className="w-6 h-6 object-cover object-left"
-                src={greekFlag}
-                alt="search"
-              />
-              <span>ΕΛ</span>
-              <Image src={downArrow} alt="dropdown icon arrow" />
+            <div className="w-full flex flex-row justify-center items-center gap-4 py-4">
+              <button>
+                <Image src={search} alt="search-bar icon" />
+              </button>
+              <div className="flex gap-1">
+                <Image
+                  className="w-6 h-6 object-cover object-left"
+                  src={greekFlag}
+                  alt="search"
+                />
+                <span>ΕΛ</span>
+                <Image src={downArrow} alt="dropdown icon arrow" />
+              </div>
             </div>
           </>
         )}
